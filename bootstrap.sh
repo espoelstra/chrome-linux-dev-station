2f71b0407e45c93e2f15a0e7f5828a5c3dde85e3
espoelstra <7798975+espoelstra@users.noreply.github.com>
2019-04-25 01:20:04 +0000

    Revert "Comment out ssh enable as the setting persists between reboots but the service does NOT actually start???"
    This reverts commit b2944e9c9a068a42b21147254d6eb326de898378.
    It appears that maybe it requires multiple shutdown/startup cycles
    before the persisted "sudo systemctl enable ssh" takes effect?

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,9 +10,8 @@ case $1 in
 	-h|--help) usage; exit 0;;
 esac
 sudo rm -f /etc/ssh/sshd_not_to_be_run 2> /dev/null
-# Currently this doesn't seem to take effect when the VM is booted
-# read -p $'Do you want to have ssh auto start on VM boot?\nThis is fairly safe as it is requires setting a password for a user if you are\'t using private key authentication\ny or n\n' BOOT_ENABLE
-# if ! [[ "$BOOT_ENABLE" == "n" ]]; then sudo systemctl enable ssh; fi
+read -p $'Do you want to have ssh auto start on VM boot?\nThis is fairly safe as it is requires setting a password for a user if you are\'t using private key authentication\n' BOOT_ENABLE
+! [[ "$BOOT_ENABLE" =~ n* ]] && sudo systemctl enable ssh
 sudo systemctl --no-pager status ssh
 sudo systemctl start ssh
 mkdir -p ~/.ssh/
