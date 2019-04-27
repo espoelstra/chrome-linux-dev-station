ddaf6041d00829c57bc909ea4abf2137e4f41651
espoelstra <7798975+espoelstra@users.noreply.github.com>
2019-04-27 03:59:10 +0000

    Updated directions with better way to get SSH key without an extra extension installation

--- README.md
+++ README.md
@@ -4,8 +4,8 @@
 
 ### Disclaimer
 
-You can `curl` and run the script directly, but the Linux VM has passwordless `sudo`, so before you hose up that VM (easy enough to recreate and isolated from your host, but still silly to break so easily), be smart and clone the repo and read the script first, or at least download the raw script file and double check the contents before you `bash bootstrap.sh` or `chmod +x bootstrap.sh && ./bootstrap.sh`.
+You can `curl` and run the script directly, but the Linux VM has passwordless `sudo`, so before you hose up your system (even if it easy enough to recreate and isolated from your host, it is still silly to break so carelessly), be smart and clone the repo and read the script first, or at least download the raw script file and double check the contents before you `bash bootstrap.sh` or `chmod +x bootstrap.sh && ./bootstrap.sh`.
 
 ### Requirements
 
-This bootstrap hopes you are using a Yubikey with a GPG key so you can keep it secure but also use it for SSH with a couple awesome extensions from the Chrome Webstore. The Yubikey requires setup on a "real" Linux/macOS/Windows machine until arbitrary USB devices can be passed through to the Linux VM wily-nily, see the Dr. Duh Yubikey guide for that portion. The Chrome extensions can also work on a Linux/macOS/Windows machine with the Secure Shell app so that portion of this guide might be more broadly applicable.
+This bootstrap hopes you are using a Yubikey with a GPG key so you can keep your private keys VERY secure but also able to use it for SSH with a couple awesome extensions from the Chrome Webstore. The Yubikey requires setup on a "real" Linux/macOS/Windows machine until arbitrary USB devices can be passed through to the Linux VM wily-nily, see the Dr. Duh Yubikey guide for that portion. The Chrome extensions can also work on a Linux/macOS/Windows machine with the Secure Shell app so that portion of this guide might be more broadly applicable.
--- bootstrap.sh
+++ bootstrap.sh
@@ -9,11 +9,29 @@ EOH
 case $1 in
 	-h|--help) usage; exit 0;;
 esac
+echo "Removing file that doesn't allow ssh service to start"
 sudo rm -f /etc/ssh/sshd_not_to_be_run 2> /dev/null
-read -p $'Do you want to have ssh auto start on VM boot?\nThis is fairly safe as it is requires setting a password for a user if you are\'t using private key authentication\n' BOOT_ENABLE
-! [[ "$BOOT_ENABLE" =~ n* ]] && sudo systemctl enable ssh
+
+read -p $'Do you want to have ssh auto start on VM boot?\nThis is fairly safe as it is requires setting a password to access the system, and one is not set for your user or root by default, plus you should ALWAYS prefer to use private key authentication over passwords\n' BOOT_ENABLE
+! [[ "$BOOT_ENABLE" =~ n* ]] && sudo systemctl enable ssh && echo "Enabled SSH on VM boot"
 sudo systemctl --no-pager status ssh
-sudo systemctl start ssh
+sudo systemctl start ssh && echo "Started SSH service"
+cat <<-EOT
+1. You should install the Secure Shell (by Google) and SSH Agent for Secure Shell (by Google) and the Smartcard Connector (by Google). You may also want the Crosh Window extension to open Secure Shell as a window instead of a tab.
+2. To open Secure Shell as a window, open the Launcher fully (hit the Search key and the ^ to expand it), then locate the Secure Shell icon and right click and where it says "New tab" hit the arrow and select "New window".
+3. Now launch Secure Shell and add a new entry by filling in the fields at the bottom.
+    * You will want to use the username that was created in your VM, it matches the part of your email address before the @whateverdomain.com portion.
+    * For the hostname it should be penguin.linux.test unless you've done some advanced changes.
+    * You can leave the port empty as it defaults to 22 which is the normal SSH port.
+    * In the "SSH relay server options" add '--ssh-agent=gsc'
+    * In the "SSH arguments" add '-A'
+    * Click Connect or press Enter
+    * You will get prompted for a password, DON'T PANIC, you haven't (and shouldn't) set one, this is OK, just hit Enter each time it asks until the connection fails
+    * Press Ctrl+Shift+J to open the Javascript console, if you have a Yubikey/smartcard inserted and you have configured a GPG key on it, you should see the SSH public key that needs placed into the ~/.ssh/authorized_keys file in this console, copy it and paste it into the Terminal of the Linux VM and hit Enter and then try Connect again, you should get prompted for your Yubikey's PIN, and you can decide whether to cache the PIN or not, and then BAM, you should be in your VM with the MAJOR benefit of having your SSH public key courtesy of the -A option provided to the SSH agent.
+EOT
+
 mkdir -p ~/.ssh/
-read -s -p $'Paste your SSH public key from smart-ssh\n' PUBKEY
-echo $PUBKEY | tee -a ~/.ssh/authorized_keys
+
+read -p $'Paste your SSH public key found in the Javascript console\n' PUBKEY
+echo $PUBKEY >> ~/.ssh/authorized_keys
+echo "Setup complete, you should now be able to connect without a password when your Yubikey is plugged in"
